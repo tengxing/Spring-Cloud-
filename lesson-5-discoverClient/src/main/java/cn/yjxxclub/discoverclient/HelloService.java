73fe4f5bdf26b3db46930d258c881aa8b1541b0b
tengxing <tengxing7452@163.com>
2018-03-06 16:22:37 +0800

    服务消费+Feign

---
+++ lesson-5-discoverClient/src/main/java/cn/yjxxclub/discoverclient/HelloService.java
@@ -0,0 +1,17 @@
+package cn.yjxxclub.discoverclient;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author X.Teng
+ * @email tengxing7452@163.com
+ * @date 18-3-6 下午3:45
+ */
+@FeignClient(name = "helloService")
+public interface HelloService {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    String sayHello(@RequestParam(value = "name") String name);
+}
